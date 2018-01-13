f14d2199ea9541143f9b8a8fdcc790359302d9eb
Alex <alexanderlperez@gmail.com>
2018-01-13 15:39:40 -0700

    coreui integration

---
+++ src/components/Sidebar/_nav.js
@@ -0,0 +1,18 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+    {
+      name: 'Test Page',
+    url: '/testpage',
+      icon: 'icon-speedometer',
+    }
+  ]
+};
---
+++ src/views/TestPage/TestPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
+
+class TestPage extends Component {
+  render() {
+    return (
+      <div className="animated fadeIn">
+          <Alert color="danger">
+              AHHHHHHHHHH! ALERT!!
+          </Alert>
+      </div>
+    )
+  }
+}
+
+export default TestPage;
