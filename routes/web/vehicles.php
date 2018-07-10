2b728094aff569e3ae0b18d06c94d78e1f450a50
codypraveen <cody.praveen@ithands.net>
2018-07-10 15:56:03 +0530

    Added vehicle api

--- routes/web/vehicles.php
+++ routes/web/vehicles.php
@@ -1,5 +1,6 @@
 <?php
-Route::group(array('namespace' => 'Dealer', 'middleware' => ['auth', 'role:'.config('constants.role.dealer')]), function () {
+Route::group(array('namespace' => 'Dealer', 'middleware' => ['auth', 'role:'.config('constants.role.super_admin').'|'.config('constants.role.dealer')]), function () {
 
     Route::resource('vehicles', 'VehicleController');
+    Route::get('getVehicles', 'VehicleController@getData')->name('getVehicles');
 });
