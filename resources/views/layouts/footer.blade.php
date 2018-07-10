6a8bda1a3702d6b6f971d291dfa57190adcab532
codypraveen <cody.praveen@ithands.net>
2018-07-10 16:15:39 +0530

    Fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,3 @@
-<!-- footer starts here -->
-<footer>
-    <div class="container">
-
-    </div>
-</footer>
-
 <script type='text/javascript' src='{{mix('js/app.js')}}'></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,7 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <title>Laravel</title>
-        
-        
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+@extends('layouts.app')
+@section('title','Login')
 
-       
-    </head>
-    <body>
+@section('content')
         <div class="flex-center position-ref full-height">
             <div class="content" id="root">
                 <div class="title m-b-md">
@@ -22,7 +9,4 @@
                 </div>
             </div>
         </div>
-
-        <script src="{{asset('js/app.js')}}"></script>
-    </body>
-</html>
+@endsection
