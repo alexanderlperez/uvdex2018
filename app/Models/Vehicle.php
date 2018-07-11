2cc4d31c339f2099c8d624fb7cbfa242e6a5b113
codypraveen <cody.praveen@ithands.net>
2018-07-11 15:58:15 +0530

    Added exclude scope

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -12,4 +12,11 @@ class Vehicle extends Model
      * @var array
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+
+    public function scopeExclude($query,$value = array())
+    {
+        $columns = $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
+        return $query->select( array_diff( $columns,(array) $value) );
+    }
 }
