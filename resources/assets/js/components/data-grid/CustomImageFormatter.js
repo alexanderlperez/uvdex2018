255501f3c5369a2be99dbb6f871d929705697b50
codypraveen <cody.praveen@ithands.net>
2018-08-02 16:01:41 +0530

    Added images count

--- resources/assets/js/components/data-grid/CustomImageFormatter.js
+++ resources/assets/js/components/data-grid/CustomImageFormatter.js
@@ -3,7 +3,11 @@ import React from "react";
 class CustomImageFormatter extends React.Component {
 
     render() {
-        return (<div><img src={this.props.value} height={40} width={40}/> ({this.props.dependentValues.images_count})</div>);
+
+        if (this.props.dependentValues.images_count)
+            return (<div><img src={this.props.value} height={40} width={40}/> ({this.props.dependentValues.images_count})</div>);
+        else
+            return (<div><img src={null} height={40} width={40}/> ({this.props.dependentValues.images_count})</div>);
     }
 }
 
