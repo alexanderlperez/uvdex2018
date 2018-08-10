556c1ee67770a894d377586df3adf1ce23c425c9
codypraveen <cody.praveen@ithands.net>
2018-08-10 18:28:21 +0530

    Updated images gallery

--- resources/assets/js/components/GallerySlider/Slider.js
+++ resources/assets/js/components/GallerySlider/Slider.js
@@ -1,91 +1,65 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
 import '../../royalslider.min.js';
-import $ from 'jquery';
-import jQuery from 'jquery';
-
-
 
 class Slider extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            images: [],
-        };
+        this.state = { images: [] };
     }
 
-     componentWillMount() {
-        this.setState({images: [
-            {
-                id: 1,
-                img: '/images/img1.jpeg',
-            },
-            {
-                id: 2,
-                img: '/images/img2.jpeg',
-            },
-            {
-                id: 3,
-                img: '/images/img3.jpeg',
-            },
-            {
-                id: 4,
-                img: '/images/img4.jpeg',
-            },
-            {
-                id: 5,
-                img: '/images/img5.jpeg',
-            },
-            {
-                id: 6,
-                img: '/images/img6.jpeg',
-            },
-        ]});
+    componentWillReceiveProps(nextProps){
+        this.setState({ images: nextProps.images });
     }
 
+    componentDidUpdate(){
 
-    componentDidMount() {
         $(".royalSlider").royalSlider({
             controlNavigation: 'thumbnails',
-				imageScaleMode: 'fit-if-smaller',
-				navigateByClick: true,
-				arrowsNav: true,
-				arrowsNavAutoHide: true,
-				arrowsNavHideOnTouch: true,
-				keyboardNavEnabled: true,
-				fadeinLoadedSlide: true,
-				imageScalePadding: 4,
-				thumbs:{
-                    appendSpan: true,
-					firstMargin: true,
-					paddingBottom: 4,
-                },
-					
-				fullscreen:{
-                    enabled: true,
-					nativeFS: true
-                }
+            imageScaleMode: 'fit-if-smaller',
+            navigateByClick: true,
+            arrowsNav: true,
+            arrowsNavAutoHide: true,
+            arrowsNavHideOnTouch: true,
+            keyboardNavEnabled: true,
+            fadeinLoadedSlide: true,
+            imageScalePadding: 4,
+            thumbs:{
+                appendSpan: true,
+                firstMargin: true,
+                paddingBottom: 4,
+            },
+
+            fullscreen:{
+                enabled: true,
+                nativeFS: true
+            }
         });
+
         $(".rsFullscreenIcn").click();
-
     }
-    
-    render(){
-        return(
-            <div className="royalSlider rsDefault"> 
-                {this.state.images.map(imgItem => {
 
-                    return(
-                        <a class="rsImg"   data-rsBigImg={imgItem.img} href={imgItem.img}>
-                            <img  class="rsTmb" src={imgItem.img} alt=""/></a>
-                    )
-                })}           
-                
-                
-            </div>
-        )
-        
+
+    render(){
+
+        if (this.state.images.length > 0) {
+
+            return (
+                <div className="royalSlider rsDefault">
+                    {
+                        this.state.images.map((img, i) => {
+                            return (
+                                <React.Fragment key={i}>
+                                    <a className="rsImg" data-rsbigimg={img} href={img}>
+                                        <img className="rsTmb" src={img} alt=""/></a>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </div>
+            )
+        } else
+            return ( <div className="royalSlider rsDefault"> </div> );
+
     }
 }
 
