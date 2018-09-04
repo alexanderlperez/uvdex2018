6a1a37dba611634c2ff9fc6db9b5cae048d76dbf
codypraveen <cody.praveen@ithands.net>
2018-09-04 14:31:09 +0530

    Removed content policy

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -18,7 +18,6 @@ class SecureHeaders
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Strict-Transport-Security', 'max-age:31536000; includeSubDomains');
-        $response->headers->set('Content-Security-Policy', "style-src 'self'"); // Clearly, you will be more elaborate here.
         return $response;
     }
     private function removeUnwantedHeaders($headerList)
