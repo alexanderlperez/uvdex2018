dcbf8d07d6e15088e9c5db6953168cda02b0ab27
codypraveen <cody.praveen@ithands.net>
2018-09-04 14:46:25 +0530

    Removed xframe

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -16,7 +16,6 @@ class SecureHeaders
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
-        $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Strict-Transport-Security', 'max-age:31536000; includeSubDomains');
         return $response;
     }
